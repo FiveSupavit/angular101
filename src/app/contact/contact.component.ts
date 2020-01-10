c5f6f2f80e639ea9c62551d96c361cd7fa78ff8f
FiveSupavit <fivexoxo@hotmil.com>
2020-01-10 15:03:25 +0700

    Contact -> Change body

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -6,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public data:any=[]
 
   constructor() { }
+
+  save(name, email, mobile, subject, message): void {
+    this.data['name']= name;
+              this.data['email']= email;
+              this.data['mobile']= mobile;
+              this.data['subject']= subject;
+              this.data['message']= message;
+    console.log(this.data);
+                //add request to send email or into mysql
+      //           this.http.put<any>('http://localhost/api/v1/update/', this.data).subscribe(
+      //   res => {
+      //     console.log(res);
+      // },
+      // (err: HttpErrorResponse) => {
+      //   if (err.error instanceof Error) {
+      //     console.log("Client-side error occured.");
+      //   } else {
+      //     console.log("Server-side error occurred.");
+      //   }
+      // });
+   }
 
   ngOnInit() {
   }
